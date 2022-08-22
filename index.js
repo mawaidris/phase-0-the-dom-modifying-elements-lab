a35d64a9050e6809293975563453d0e697758111
mawa <mawahama05@gmail.com>
2022-08-21 22:35:19 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+document.querySelector('main#main').remove();
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+newHeader.innerHTML = 'Mawa is the champion';
+// document.getElementsByTagName("h1").innerHTML = 'is the champion'
+                                  
+
